399a8d9e02648c0cf22d560c85b5b9c4128b8a5b
Aurélien Rainone <aurelien.rainone@gmail.com>
2024-12-06 14:58:29 +0100

    Code review

--- main.go
+++ main.go
@@ -25,17 +25,23 @@ func main() {
 	in := flag.Arg(0)
 	out := flag.Arg(1)
 
-	data, err := getIn(in)
+	indata, err := getIn(in)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	res, err := countWords(data)
+	// TODO come per getIn, fare una funzione getOut che astrae la destinazione.
+
+	res, err := countWords(indata)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	w := sortWords(res, *reverse)
+
+	// TODO: usare json.NewEncoder per la codifica a json, cosi puoi passare il io.Writer ottenuto da getOut
+	// enc, err := json.NewEncoder(w)
+	// enc.Encode(w)
 
 	if *j {
 		res, err := json.MarshalIndent(w, "", "  ")
@@ -53,10 +59,11 @@ func main() {
 func getIn(in string) (data io.Reader, err error) {
 	if in == "-" {
 		return os.Stdin, nil
-	} else {
+	} else /*TODO: else da rimuovere*/ {
+		// TODO: file.Close
 		data, err := os.Open(in)
 		if err != nil {
-			return nil, err
+			return nil, fmt.Errorf("can't open input file: %s", err)
 		}
 		return data, nil
 	}
@@ -96,6 +103,7 @@ func outResult(outArg string, content string) {
 	if outArg != "" {
 		file, err := os.Create(outArg)
 		if err != nil {
+			// TODO: invece di log.Fatal, meglio fare return del'error e gestirlo nel main
 			log.Fatalf("can't create file: %v", err)
 		}
 		defer file.Close()
